c120bb6aa7ab433404d6dee2fd4a12c30046d3c1
jayasurya88 <jayasurya7879@gmail.com>
2024-10-27 21:45:30 +0530

    nothing

--- socialcore/socialcore/settings.py
+++ socialcore/socialcore/settings.py
@@ -148,5 +148,9 @@ AUTH_USER_MODEL = 'core.CustomUser'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
